3e7546d0477602e82ad5b223535e57fd1a087d6c
tomhoon <gnsdl9108@naver.com>
2025-06-03 21:59:15 +0900

    feat: 공통 1280 제작

--- app/page.js
+++ app/page.js
@@ -1,8 +1,20 @@
+import C_1280 from "@/common/pages/C_1280";
 
 export default function Home() {
   return (
-    <div>
+    /** 헤더 예시 */
+    /** 
+     <Header>
+       <C_1280>
       
-    </div>
+       </C_1280>
+     </Header>
+    */
+    /** 바디 예시 */
+    <C_1280>
+      {/* 바디내용 */}
+      {/* 푸터 */}
+    </C_1280>
+
   );
 }
