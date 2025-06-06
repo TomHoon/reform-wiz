8de4163f145af335b1962d2d6a5c78727087995d
TomHoon <99778185+TomHoon@users.noreply.github.com>
2025-06-06 11:51:23 +0900

    공통 컴포넌트 Header 제작 (#3)

--- app/page.js
+++ app/page.js
@@ -1,30 +1,9 @@
-import C_1280 from "@/common/pages/C_1280";
+import Header from "@/common/molecules/header/Header";
 
 export default function Home() {
   return (
-    /** 헤더 예시 */
-    /** 
-     <Header>
-       <C_1280>
-      
-       </C_1280>
-     </Header>
-    */
-
-
-    /** 바디 예시 */
-    <C_1280>
-      {/* 작업내용 */}
-    </C_1280>
-
-    /**
-     푸터예시
-     <Footer>
-      <C_1280>
-        작업내용
-      </C_1280>
-     </Footer> 
-     */
+    /** 헤더 */
+    <Header/>
 
   );
 }
