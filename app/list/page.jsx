04e70e7cc0224c302c357541f6cc7b148e432432
tomhoon <gnsdl9108@naver.com>
2025-06-20 13:48:14 +0900

    fix: merge 잘못되어 이전 소스 들어가있는 내용 제거

--- app/list/page.jsx
+++ app/list/page.jsx
@@ -2,11 +2,6 @@ import SearchBar from '@/common/molecules/list/SearchBar';
 import List from '@/common/molecules/list/List';
 import Paging from '@/common/molecules/list/Paging';
 
-export const dynamic = 'force-dynamic';
-import SearchBar from '@/common/molecules/list/SearchBar';
-import List from '@/common/molecules/list/List';
-import Paging from '@/common/molecules/list/Paging';
-
 export default async function ListPage({ searchParams }) {
   const { page = 1, size = 9, location = '강동구' } = await searchParams;
 
