3aa4ebb4035245fa8f531c567f4bc25ebcd6568a
tomhoon <gnsdl9108@naver.com>
2025-07-22 23:59:14 +0900

    feat: boardWrite Style 작업전

---
+++ app/write/layout.jsx
@@ -0,0 +1,16 @@
+import Header from "@/common/molecules/header/Header";
+import Footer from "@/common/molecules/footer/Footer";
+import C_1280 from "@/common/pages/C_1280";
+
+
+export default function ListLayout({ children }) {
+ return (
+  <>
+   <C_1280>
+    <Header />
+    {children}
+   </C_1280>
+   <Footer />
+  </>
+ );
+}
