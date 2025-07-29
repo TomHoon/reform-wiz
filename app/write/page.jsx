84213b2eaee5e2e3f8ea7a3047c633b1b3124c44
tomhoon <gnsdl9108@naver.com>
2025-07-29 22:59:55 +0900

    등록하기 css 추가

--- app/write/page.jsx
+++ app/write/page.jsx
@@ -5,14 +5,20 @@ export default async function Write() {
  return (
   <div className={styles.writeContainer}>
    <h2>등록하기</h2>
-   <ul>
+   <ul className={styles.writeWrapper}>
+
+
     <li>
      <input type="file" name="" id="" />
     </li>
+
+
     <li className={styles.writeItem}>
      <label htmlFor="">가구명</label>
      <input type="text" name="" id="" />
     </li>
+
+
     <li className={styles.writeItem}>
      <label htmlFor="">카테고리명</label>
      <div className={styles.selectWrapper}>
@@ -25,24 +31,30 @@ export default async function Write() {
       </select>
      </div>
     </li>
+
+
     <li className={styles.writeItem}>
      <label htmlFor="">리폼내용</label>
-     <textarea name="" id="" placeholder="리폼할 내용을 자유롭게 입력해주세요"></textarea>
+     <textarea className={styles.writeTx} name="" id="" placeholder="리폼할 내용을 자유롭게 입력해주세요"></textarea>
     </li>
-    <li>
+
+
+    <li className={styles.writeItem}>
      <label htmlFor="">설치장소</label>
      <div className={styles.addressWrapper}>
       <div className={styles.addressNumArea}>
-       <input type="text" name="" id="" placeholder="우편번호" />
+       <input className={styles.addressMain} type="text" name="" id="" placeholder="우편번호" />
        <button>우편번호 찾기</button>
       </div>
-      <input type="text" name="" id="" placeholder="도로명주소" />
-      <input type="text" name="" id="" placeholder="상세주소" />
+      <input className={styles.addressSub} type="text" name="" id="" placeholder="도로명주소" />
+      <input className={styles.addressSub} type="text" name="" id="" placeholder="상세주소" />
      </div>
     </li>
+
+
    </ul>
 
-   <button>등록하기</button>
+   <button className={styles.writeBtn}>등록하기</button>
   </div>
  );
 }
