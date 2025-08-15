503b17185c96c4eb218d215566ae38456f9ca7e2
tomhoon <gnsdl9108@naver.com>
2025-08-16 00:31:56 +0900

    로컬이미지 사용허용처리 & env 환경변수설정

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    API_URL: process.env.API_URL,
+  },
+  images: {
+    domains: ['localhost'], // add your hostname(s) here
+  },
+};
 
 export default nextConfig;
