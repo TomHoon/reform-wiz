a5b6510908282d3fa22f170ae3987c51f8c2e4d1
tomhoon <gnsdl9108@naver.com>
2025-08-16 14:36:54 +0900

    로그인 페이지 퍼블 추가

---
+++ app/login/page.jsx
@@ -0,0 +1,67 @@
+'use client'
+
+import styles from '@/styles/login/Login.module.scss';
+import { useEffect, useState } from 'react';
+
+export default function Login() {
+ const [activeType, setActiveType] = useState('일반');
+ const [pressed, setPressed] = useState(false);
+
+ const goLogin = () => {
+  setPressed(true);
+
+  setTimeout(() => {
+   setPressed(false);
+  }, 200);
+
+ }
+
+ return (
+  <div className={styles.outerContainer}>
+
+   <h2>로그인</h2>
+
+   <div className={styles.innerContainer}>
+
+    <div className={styles.typeButtons}>
+
+     <div className={`${activeType === '일반' && styles.active}`} onClick={() => setActiveType('일반')}>
+      <span>일반회원</span>
+     </div>
+
+     <div className={`${activeType === '업체' && styles.active}`} onClick={() => setActiveType('업체')}>
+      <span>업체</span>
+     </div>
+    </div>
+
+    <div className={styles.inputArea}>
+     <input type="text" placeholder='아이디' />
+     <input type="password" placeholder='비밀번호' />
+     {/* <img src="/icons/eye.png" alt="" /> */}
+     <img src="/icons/eye-slash.png" alt="" />
+     <button className={`${pressed && styles.pressed}`} onClick={() => goLogin()}>로그인</button>
+    </div>
+
+    <hr className={styles.splitter} />
+
+    <div className={styles.socialArea}>
+     <button className={styles.kakaoLogin}>
+      <img src="/icons/kakao.png" alt="" />
+      카카오로그인
+     </button>
+     <button className={styles.googleLogin}>
+      <img src="/icons/google.png" alt="" />
+      구글로그인
+     </button>
+    </div>
+
+
+    <div className={styles.more}>
+     <button>회원가입</button>
+     <button>아이디/비밀번호 찾기</button>
+    </div>
+
+   </div>
+  </div>
+ )
+}
