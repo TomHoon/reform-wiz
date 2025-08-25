3143716ca1fdc7bfa173b7bf2b60dfc9e38bcc0a
TomHoon <99778185+TomHoon@users.noreply.github.com>
2025-08-26 01:06:21 +0900

    fix: 로그인 유지 (#15)

--- common/molecules/header/LoginButtonGroup.jsx
+++ common/molecules/header/LoginButtonGroup.jsx
@@ -3,16 +3,49 @@
 import styles from "@/styles/C_Header.module.scss"
 
 export default function LoginButtonGroup() {
+ const isLogin = localStorage.getItem("isLogin");
+
+ const loginCb = async () => {
+  if (isLogin) {
+   localStorage.removeItem("isLogin");
+
+   const res = await fetch(`${process.env.API_URL}/api/v1/member/logout`, {
+    headers: {
+     'Content-Type': 'application/json'
+    },
+    method: 'POST',
+    credentials: "include", // ✅ crucial
+
+   });
+   const json = await res.json();
+
+   if (json.status === 'success') {
+    location.href = "/";
+   } else {
+    alert("로그아웃실패");
+   }
+
+
+  } else {
+   location.href = "/login";
+  }
+ }
+
  return (
   <div className={styles.loginBtnGroup}>
    <ul>
 
     <li>
-     <button onClick={() => loginCb()} className={styles.typeA}>로그인</button>
+     <button onClick={() => loginCb()} className={styles.typeA}>{isLogin ? '로그아웃' : '로그인'}</button>
     </li>
-    <li>
-     <button onClick={() => joinCb()} className={styles.typeB}>회원가입</button>
-    </li>
+    {
+     !isLogin
+     &&
+     <li>
+      <button onClick={() => joinCb()} className={styles.typeB}>회원가입</button>
+     </li>
+    }
+
 
    </ul>
   </div>
